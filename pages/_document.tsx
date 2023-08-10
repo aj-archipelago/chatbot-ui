27ccdf7f90f004c311a5dac13e5b99b787743bf1
Jason McCartney <jmac@theroot.org>
2023-08-10 10:35:55 -0700

    Implementing RTL direction

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { DocumentProps, Head, Html, Main, NextScript } from 'next/document';
-
 import i18nextConfig from '../next-i18next.config';
 
 type Props = DocumentProps & {
@@ -9,8 +8,10 @@ type Props = DocumentProps & {
 export default function Document(props: Props) {
   const currentLocale =
     props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
+  const dir = currentLocale === 'ar' ? 'rtl' : 'ltr';
+
   return (
-    <Html lang={currentLocale}>
+    <Html lang={currentLocale} dir={dir}>
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
